e63fa2e65ddfb2a9ca2b3a080d60d1f39d438251
Bruno José Vilas Bôas <bruuno28.28@gmail.com>
2026-06-01 08:43:02 -0300

    DepartmentList Controller

--- src/module-info.java
+++ src/module-info.java
@@ -1,10 +1,11 @@
 module workshop {
     requires javafx.controls;
     requires javafx.fxml;
-	requires javafx.graphics;
+    requires javafx.graphics;
     
     opens gui to javafx.fxml;
     opens application to javafx.fxml;
+    opens model.entities to javafx.base;  // ← NOVO! Para o TableView funcionar
     
     exports application;
 }
