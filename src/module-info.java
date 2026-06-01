1df71a9ec51a85858d1014933781f60adbc529ba
Bruno José Vilas Bôas <bruuno28.28@gmail.com>
2026-06-01 19:26:14 -0300

    adiconando banco de dados

--- src/module-info.java
+++ src/module-info.java
@@ -2,10 +2,11 @@ module workshop {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.sql;  // ← ADICIONAR ISSO!
     
     opens gui to javafx.fxml;
     opens application to javafx.fxml;
-    opens model.entities to javafx.base;  // ← NOVO! Para o TableView funcionar
+    opens model.entities to javafx.base;
     
     exports application;
 }
