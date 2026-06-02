6172c5fec914515d00f061b2be5a30f3dd0d0495
Bruno José Vilas Bôas <bruuno28.28@gmail.com>
2026-06-02 11:17:40 -0300

    Observe desing pattern to update

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -35,7 +35,7 @@ public class MainViewController implements Initializable {
 	public void onMenuItemDepartmentAction() {
 		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
 			controller.setDepartmentService(new DepartmentService());
-			controller.updatetableView();
+			controller.updateTableView();
 		});
 	}
 	
